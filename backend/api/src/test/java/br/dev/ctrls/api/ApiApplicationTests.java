c4c204c365de272bc2879a83fb31a08598005822
VictorGHss <vg.hss0101@gmail.com>
2025-10-17 14:23:02 -0300

    fix: ApiApplicationTests now extends to BaseIntegrationTest

--- backend/api/src/test/java/br/dev/ctrls/api/ApiApplicationTests.java
+++ backend/api/src/test/java/br/dev/ctrls/api/ApiApplicationTests.java
@@ -2,9 +2,9 @@ package br.dev.ctrls.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import br.dev.ctrls.api.integration.BaseIntegrationTest;
 
-@SpringBootTest
-class ApiApplicationTests {
+class ApiApplicationTests extends BaseIntegrationTest {
 
     @Test
     void contextLoads() {
